b8c463e97fe74868c8fa992ccfe602f497d33ce8
katoken-0215 <katoken0215@gmail.com>
2026-06-13 22:10:35 +0900

    feat: add smart-cmd-pick-repo helper

---
+++ test/test_smart-cmd-pick-repo.zsh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+local here=${0:A:h}
+source "$here/helper.zsh"
+source "$here/../autoload/smart-cmd-pick-repo"
+
+# --- 正常系: 選択されたリポジトリのフルパスを返す ---
+ghq() {
+	case $1 in
+		list) print -l "github.com/foo/bar" "github.com/baz/qux" ;;
+		root) print -- "/Users/test/ghq" ;;
+	esac
+}
+fzf() { print -- "github.com/foo/bar" }
+
+local result
+result=$(smart-cmd-pick-repo)
+assert_eq "/Users/test/ghq/github.com/foo/bar" "$result" "選択リポジトリのフルパスを返す"
+
+# --- キャンセル系: fzfが非0なら非0で返す ---
+fzf() { return 130 }
+smart-cmd-pick-repo >/dev/null
+assert_eq 1 $? "fzfキャンセル時は非0で返す"
+
+test_summary
