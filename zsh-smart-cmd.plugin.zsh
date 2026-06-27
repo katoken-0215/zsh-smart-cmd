6c54cbe5b86c573ccf3605bbe281ba31d762bcff
Kenji KATO <532265+katoken-0215@users.noreply.github.com>
2026-06-27 14:47:12 +0900

    feat: add smart-history command with macOS compatibility (#5)

--- zsh-smart-cmd.plugin.zsh
+++ zsh-smart-cmd.plugin.zsh
@@ -5,4 +5,4 @@ fpath=(
 $fpath
 )
 
-autoload -Uz cc-haiku cc-sonnet cc-opus new new-term new-cc smart-cmd-pick-repo
+autoload -Uz cc-haiku cc-sonnet cc-opus new new-term new-cc smart-cmd-pick-repo smart-history
